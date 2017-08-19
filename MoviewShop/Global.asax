780bdb6c86d2f4ee818bfd9257a9fc37058456f9
Anatoliy Pronyk <apronyk@intetics.com>
2017-08-19 13:21:29 +0300

    Add project files.

---
+++ MoviewShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MoviewShop.MvcApplication" Language="C#" %>
---
+++ MoviewShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoviewShop.Startup))]
+namespace MoviewShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
